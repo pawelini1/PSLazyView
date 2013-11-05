0b1f51dd6e9bdbcb5ceeee72da0b12ffe6afe134
Paweł Szymański <pawel.szymanski.lfc@gmail.com>
2013-11-06 00:36:05 +0100

    - building sample project environment

---
+++ PSLazyViewExample/PSSlowView.m
@@ -0,0 +1,72 @@
+//
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2013 Paweł Szymański
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy of
+//    this software and associated documentation files (the "Software"), to deal in
+//    the Software without restriction, including without limitation the rights to
+//    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//    the Software, and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "PSSlowView.h"
+
+@implementation PSSlowView
+
+@synthesize allPoints = _allPoints;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _allPoints = [self generateRandomPoints];
+    }
+    return self;
+}
+
+-(NSArray*)generateRandomPoints{
+    NSMutableArray* points = [NSMutableArray array];
+    for (int i = 0; i < PS_POINTS_COUNT; i++) {
+        CGPoint point = CGPointMake(arc4random() % (int)CGRectGetWidth(self.frame),
+                                    arc4random() % (int)CGRectGetHeight(self.frame));
+        [points addObject:[NSValue valueWithCGPoint:point]];
+    }
+    return points;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    
+    CGContextSetLineWidth(context, 2.0);
+    
+    if (self.allPoints.count > 0) {
+        CGPoint point = ((NSValue*)self.allPoints[0]).CGPointValue;
+        CGContextMoveToPoint(context, point.x, point.y);
+        
+        NSInteger count = self.allPoints.count;
+        for (int i = 1; i < count; i++) {
+            CGPoint point = ((NSValue*)self.allPoints[i]).CGPointValue;
+            CGContextAddLineToPoint(context, point.x, point.y);
+        }
+    }
+    
+    CGContextStrokePath(context);
+}
+
+@end
